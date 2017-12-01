8a9757bdbeaed98dbc981ac58b08f7eae5cff0a5
Logan Korn <Logan.Korn@carvana.com>
2017-12-01 08:24:39 -0700

    my first message

--- Carvana.Fizzbuzz/Program.cs
+++ Carvana.Fizzbuzz/Program.cs
@@ -2,11 +2,30 @@
 
 namespace Carvana.Fizzbuzz
 {
-    class Program
+    public class Program
     {
         static void Main()
         {
-            Console.WriteLine("Practice 01");
+            Console.WriteLine("I dare you to enter in a number!");
+           int number = Convert.ToInt32(Console.ReadLine());
+            var value = PickValue(number);
+            Console.WriteLine(value);
+            Console.ReadLine();
         }
+
+        public static string PickValue(int number)
+        {
+            int remainder = number % 3;
+            if (remainder == 0)
+            {
+                return "Fizz";
+            }
+            else
+            {
+               return number.ToString();
+            }
+
+        }
+
     }
 }
